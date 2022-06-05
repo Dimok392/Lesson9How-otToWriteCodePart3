56347c4920f9d464701ada5f72660adff90a1c80
Dimok <dimok392@mail.ru>
2022-06-05 22:44:53 +0400

    example004

---
+++ example/example004/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+Console.WriteLine(" Введите первое число");
+int n = int.Parse(Console.ReadLine());
+int SummNumbers(int n)
+{
+    if (n == 0) return 0;
+    Console.Write(n%10 + " ");
+    int summa=SummNumbers(n/10);
+    return summa+n%10 ;
+}
+int summ = SummNumbers(n);
+Console.WriteLine(summ);
