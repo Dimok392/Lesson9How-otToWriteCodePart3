9b49787237ed9ca336eb33e1f6f14a8c14ef2535
Dimok <dimok392@mail.ru>
2022-06-05 23:14:45 +0400

    example005

---
+++ example/example005/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 69: Напишите программу, которая на вход принимает два числа A и B, и
+// возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8 
+
+
+Console.WriteLine(" Введите первое число");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine(" Введите первое число");
+int b = int.Parse(Console.ReadLine());
+int ProizNumbers(int a,int b)
+{
+    if (b<1) return 1;
+    return a*ProizNumbers(a,b-1);
+}
+int proiz = ProizNumbers(a,b);
+Console.WriteLine(proiz);
